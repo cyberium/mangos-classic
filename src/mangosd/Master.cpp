a5162e9029d7bc25d986f92eda0145b710195c3c
Cala <calaftp@free.fr>
2017-05-17 17:12:23 +0200

    Playerbot: tag playerbot code
    Tag Playerbot code embedded in core for next steps by finding them
    easily

--- src/mangosd/Master.cpp
+++ src/mangosd/Master.cpp
@@ -36,6 +36,7 @@
 #include "RASocket.h"
 #include "Util.h"
 #include "revision_sql.h"
+// ------ Playerbot mod ------ //
 #include "playerbot/revision_sql.h"
 #include "MaNGOSsoap.h"
 #include "Mails/MassMailMgr.h"
