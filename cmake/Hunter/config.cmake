ae7d74f9b11a29718f1cd99743a01da4acc32c48
Cyberium <cyberium@users.noreply.github.com>
2019-12-19 19:00:23 +0100

    Restore back zlib 1.2.11-p1 but switch order with MySQL-Client package
    Look like MySQL-Client build a version of zlib if not already installed.

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -2,7 +2,7 @@
 # its important so every users will have same lib versions
 
 hunter_config(Boost VERSION 1.71.0-p0)
-hunter_config(ZLIB VERSION 1.2.11-p0)
+hunter_config(ZLIB VERSION 1.2.11-p1)
 hunter_config(BZip2 VERSION 1.0.8-p0)
 hunter_config(MySQL-client VERSION 6.1.9-p1)
 
